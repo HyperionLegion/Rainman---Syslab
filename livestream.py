9a5c8bf62c08557cd573e69a337740108b2afedf
Kim <1496326@fcps.edu>
2022-01-21 00:17:47 -0500

    fixed bounding box issues

--- livestream.py
+++ livestream.py
@@ -27,7 +27,7 @@ np.random.seed(42)
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
 	dtype="uint8")
 # derive the paths to the YOLO weights and model configuration
-weightsPath = os.path.sep.join([args["yolo"], "yolo-obj_5100.weights"])
+weightsPath = os.path.sep.join([args["yolo"], "yolo-obj_4100.weights"])
 configPath = os.path.sep.join([args["yolo"], "yolo-obj.cfg"])
 # load our YOLO object detector trained on COCO dataset (80 classes)
 # and determine only the *output* layer names that we need from YOLO
@@ -38,7 +38,8 @@ ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 # initialize the video stream, pointer to output video file, and
 # frame dimensions
-vs = cv2.VideoCapture(0) # replace with live stream
+vs = cv2.VideoCapture(1) # replace with live stream
+#vs = cv2.VideoCapture(args["input"])
 writer = None
 (W, H) = (None, None)
 # try to determine the total number of frames in the video file
@@ -61,11 +62,11 @@ while True:
 	print('read')
 	# if the frame was not grabbed, then we have reached the end
 	# of the stream
-	frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
 	if not grabbed:
 		break
 	# if the frame dimensions are empty, grab them
+	frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 	if W is None or H is None:
 		(H, W) = frame.shape[:2]
 # construct a blob from the input frame and then perform a forward
@@ -84,7 +85,7 @@ while True:
 	confidences = []
 	classIDs = []
 
-    # loop over each of the layer outputs
+	# loop over each of the layer outputs
 	for output in layerOutputs:
 		# loop over each of the detections
 		for detection in output:
@@ -112,7 +113,7 @@ while True:
 				boxes.append([x, y, int(width), int(height)])
 				confidences.append(float(confidence))
 				classIDs.append(classID)
-    # apply non-maxima suppression to suppress weak, overlapping
+	# apply non-maxima suppression to suppress weak, overlapping
 	# bounding boxes
 	idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
 		args["threshold"])
@@ -123,31 +124,34 @@ while True:
 			# extract the bounding box coordinates
 			(x, y) = (boxes[i][0], boxes[i][1])
 			(w, h) = (boxes[i][2], boxes[i][3])
+			print(LABELS[classIDs[i]], x, y, w, h, W, H)
 			# draw a bounding box rectangle and label on the frame
 			color = [int(c) for c in COLORS[classIDs[i]]]
-			cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+			cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
 			text = "{}: {:.4f}".format(LABELS[classIDs[i]],
 				confidences[i])
 			cv2.putText(frame, text, (x, y - 5),
 				cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-    # check if the video writer is None
-	# if writer is None:
-	# 	# initialize our video writer
-	# 	fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-	# 	writer = cv2.VideoWriter(args["output"], fourcc, 30,
-	# 		(frame.shape[1], frame.shape[0]), True)
-	# 	# some information on processing single frame
-	# 	if total > 0:
-	# 		elap = (end - start)
-	# 		print("[INFO] single frame took {:.4f} seconds".format(elap))
-	# 		print("[INFO] estimated total time to finish: {:.4f}".format(
-	# 			elap * total))
-	# # write the output frame to disk
-	# writer.write(frame)
+	# check if the video writer is None
+	if writer is None:
+		# initialize our video writer
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 30,
+			(frame.shape[1], frame.shape[0]), True)
+		# some information on processing single frame
+		if total > 0:
+			elap = (end - start)
+			print("[INFO] single frame took {:.4f} seconds".format(elap))
+			print("[INFO] estimated total time to finish: {:.4f}".format(
+				elap * total))
+	# write the output frame to disk
 	cv2.imshow('Input', frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
+	
 # release the file pointers
 print("[INFO] cleaning up...")
 writer.release()
 vs.release()
+
+#            ch = 0xFF & cv2.waitKey(1000)
